99f87782548575e65207f7a637b77557002c7b63
William <william@amovens.com>
2013-08-06 18:30:50 +0200

    Inliner funcional.
    Signed-off-by: William <william@amovens.com>

--- src/Waavi/Mailman/Mailman.php
+++ src/Waavi/Mailman/Mailman.php
@@ -1,9 +1,49 @@
 <?php namespace Waavi\Mailman;
 
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles as CssInline;
+
 class Mailman {
 
-	public function test()
+	/**
+	 *	From email address.
+	 *	@var string
+	 */
+	protected $from;
+
+	/**
+	 *
+	 *	@param \Illuminate\Foundation\Application $app
+	 */
+	public function __construct($app, $css = null)
 	{
-		echo "Waavi - Mailman";
+		$this->app = $app;
 	}
+
+	public function make($view)
+	{
+		$this->view = $view;
+		return $this;
+	}
+
+	public function from($from)
+	{
+		$this->from = $from;
+		return $this;
+	}
+
+	public function getFrom()
+	{
+		return $this->from;
+	}
+
+	public function show()
+	{
+		$html = $this->app['view']->make($this->view)->render();
+		$css = $this->app['files']->get($this->app['path.public'].'/assets/css/email.css');
+		$inliner = new CssInline($html, $css);
+		return $inliner->convert();
+		return $this->app['view']->make($this->view)->render();
+	}
+
+
 }
