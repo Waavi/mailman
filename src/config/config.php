203cfebb8e90c0aee17eefd85b6cc405cc0bd2b2
William <william@amovens.com>
2013-08-08 14:30:31 +0200

    Documented the config file.
    Signed-off-by: William <william@amovens.com>

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,25 @@
 return array(
 
 	'css'		=>	array(
+
+		/*
+		|--------------------------------------------------------------------------
+		| Email stylesheet folder.
+		|--------------------------------------------------------------------------
+		|
+		| Path relative to the public folder in which email stylesheets are stored.
+		|
+		*/
 		'folder'	=>	'/assets/css',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Email stylesheet
+		|--------------------------------------------------------------------------
+		|
+		| Default stylesheet for emails.
+		|
+		*/
 		'file'		=>	'email.css',
 	),
 );
